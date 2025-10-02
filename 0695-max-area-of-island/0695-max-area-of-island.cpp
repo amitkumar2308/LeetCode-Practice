92b41e3eddb1171def9f5ba391f5099469f5823d
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-10-02 20:56:18 +0530

    .cpp 0695-max-area-of-island Medium 10-02-2025 0 ms (100%) 27.4 MB (83.4%)

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int dfs(vector<vector<int>>& grid, int i, int j, int row, int col){
+
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j]==0)
+            return 0;
+
+        
+         
+        grid[i][j] = 0;
+        int area = 1;
+         
+        area += dfs(grid, i+1, j, row, col);
+        area += dfs(grid, i-1, j, row, col);
+        area += dfs(grid, i, j+1, row, col);
+        area += dfs(grid, i, j-1, row, col);
+
+        return area;
+
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int maxArea = 0;
+        
+        for(int i=0; i<row; i++){
+            for(int j=0;j<col; j++){
+
+                if(grid[i][j]==1){
+
+                    maxArea = max(maxArea,dfs(grid,i,j,row,col));
+
+                }
+
+            }
+        }
+    return maxArea;
+    }
+};
