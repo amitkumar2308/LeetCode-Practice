0a8fd8ef9596001b4d529dec872a0674729daa38
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-11-22 21:05:40 +0530

    .cpp 0424-longest-repeating-character-replacement Medium 11-22-2025 0 ms (100%) 10.9 MB (39.45%)

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {   
+        vector<int>freq(26,0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int res = 0;
+
+        for(int right = 0; right<s.size();right++){
+            freq[s[right]-'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right]-'A']);
+
+
+            int windowsize = right-left+1;
+
+            if(windowsize-maxFreq > k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+        res = max(res, right-left+1);
+        }
+        return res;
+        
+    }
+};
