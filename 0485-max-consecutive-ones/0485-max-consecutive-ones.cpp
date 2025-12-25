2209a2f4048fd6821facf90b410e90374921636c
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-12-25 13:57:20 +0530

    .cpp 0485-max-consecutive-ones Easy 12-25-2025 0 ms (100%) 50.1 MB (67.74%)

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int curr = 0;
+        int maxi = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1){
+                curr++;
+                maxi = max(curr,maxi);
+            }else{
+                curr = 0;
+            }
+            
+        }
+    return maxi;
+    }
+};
