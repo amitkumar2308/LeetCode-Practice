425cf4b892a074bb9c148464406d18e73a632270
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-12-25 11:40:12 +0530

    .sql 0176-second-highest-salary Medium 12-25-2025 260 ms (56.3%) 0B (100%)

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-SELECT 
-    MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee 
+WHERE salary<(SELECT MAX(salary) FROM Employee);
