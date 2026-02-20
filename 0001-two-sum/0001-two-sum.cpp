05721e940b6fcd7a4f1108ea38221762c5f38646
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-02-20 13:14:35 +0530

    .cpp 0001-two-sum Easy 02-20-2026 0 ms (100%) 14.9 MB (36.01%)

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //brute force nums[i] + nums[j] = target;
         //means nums[j] = target - nums[i];
-        unordered_map<int,int>mp; //store {key,index}
+        unordered_map<int,int> mp; //{key,index}
 
         for(int i=0; i<nums.size(); i++){
             int complement = target - nums[i];
 
             if(mp.find(complement)!=mp.end()){
-                return {mp[complement],i};
+                return{mp[complement],i};
             }
-
-        mp[nums[i]] = i;
-              
+            mp[nums[i]] = i;
         }
-
     return {};
     }
 };
