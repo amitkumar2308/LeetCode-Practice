62b6fa35df7477966a5101e4fef6de5b7f42e72f
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-02-24 18:35:48 +0530

    .cpp 0743-network-delay-time Medium 02-24-2026 83 ms (58.54%) 44.4 MB (68.39%)

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> graph(n+1); //(neighbor,weight)
-        for(auto &t:times){
-            int u = t[0],v=t[1],w=t[2];
-            graph[u].push_back({v,w});
+        vector<int>dist(n+1,INT_MAX);
+
+        unordered_map<int,vector<pair<int,int>>>adj; //src -> (target,time);
+
+        for(vector<int> &vec : times){
+            int src = vec[0];
+            int target = vec[1];
+            int time = vec[2];
+
+            adj[src].push_back({target,time});
         }
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-        pq.push({0,k});
+        queue<int>q;
+        dist[k] = 0;
+        q.push(k);
 
-        vector<int>dist(n+1,INT_MAX);
-        dist[k]=0;
+        while(!q.empty()){
 
-        while(!pq.empty()){
-            auto[time,node] = pq.top();pq.pop();
-            if(time>dist[node]) continue;
+            int u = q.front();
+            q.pop();
 
-            for(auto &[nei,w]:graph[node]){
-                if(time+w<dist[nei]){
-                    dist[nei] = time + w;
-                    pq.push({dist[nei],nei});
+            for(auto &p:adj[u]){
+                int v = p.first;
+                int wt = p.second;
+
+                if(dist[v]>dist[u]+wt){
+                    dist[v] = dist[u]+wt;
+                    q.push(v);
                 }
             }
+            
         }
-      int ans = 0;
-      for(int i=1; i<=n; i++){
-        if(dist[i]==INT_MAX) return -1;
-        ans = max(ans,dist[i]);
-      }
 
-    return ans;
+        int maxTime = 0;
+
+        for(int i=1; i<=n; i++){
+            if(dist[i]==INT_MAX) return -1;
+            maxTime = max(maxTime,dist[i]);
+        }
+        
+    return maxTime;
     }
 };
