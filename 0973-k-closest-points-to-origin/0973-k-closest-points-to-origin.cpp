daf9cdc3095f7f9296b3c48cb1f45e1471e18e23
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-04 12:40:41 +0530

    .cpp 0973-k-closest-points-to-origin Medium 05-04-2026 34 ms (70.75%) 61.7 MB (95.82%)

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>> maxHeap;
+        sort(points.begin(),points.end(),[](const vector<int>&a, const vector<int>&b){
+             return (a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]);
+        });
 
-        for (int i = 0; i < points.size(); i++) {
-            int x = points[i][0];
-            int y = points[i][1];
-
-            int distance = (x * x) + (y * y);
-
-            maxHeap.push({distance, i});
-            if (maxHeap.size() > k) {
-                maxHeap.pop();
-            }
-        }
-
-        vector<vector<int>> result;
-
-        while (!maxHeap.empty()) {
-            int value = maxHeap.top().second;
-            result.push_back(points[value]);
-            maxHeap.pop();
-        }
-        return result;
+        return vector<vector<int>>(points.begin(),points.begin()+k);
     }
 };
