6efb2b7f813268255228a4619ab72f81ca7dd8ff
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-04 14:45:58 +0530

    .cpp 0451-sort-characters-by-frequency Medium 05-04-2026 N/A (0%) N/A (0%)

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    struct Compare{
-        bool operator()(pair<char,int>& a, pair<char,int>& b){
-
-            return a.second<b.second;
-
-        }
-    };
     string frequencySort(string s) {
-           unordered_map<char,int>freq;
-           for(char c:s){
-            freq[c]++;
-           }
-
-        priority_queue<pair<char,int>, vector<pair<char,int>>, Compare>pq;
-
-        for(auto& it:freq){
-            pq.push({it.first, it.second});
-        }
-
-        string result;
-
-        while(!pq.empty()){
-            auto[ch, count] = pq.top();
-            pq.pop();
-            result.append(count,ch);
-        }
-    return result;
+       unordered_map<int,char>mp;
+       string result;
+       for(char c:s){
+        mp[c]++;
+       }
+       
+       priority_queue<pair<int,char>>pq;
+       
+       for(auto &it:mp){
+        pq.push({it.second,it.first}); //char,freq
+       }
+       
+       while(!pq.empty()){
+        auto [freq,ch] = pq.top();
+        pq.pop();
+        
+        result.append(freq,ch);
+       }
+       
+    return result;    
     }
 };
