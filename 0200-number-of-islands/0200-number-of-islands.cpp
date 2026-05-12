6c970e02cc8c11ea0a75a826651f17f2b1ed4d64
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-12 22:04:50 +0530

    .cpp 0200-number-of-islands Medium 05-12-2026 42 ms (10.18%) 25.4 MB (8.55%)

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    int rows,cols;
-    void dfs(vector<vector<char>>& grid, int r, int c){
-         if(r<0 || r>=rows || c<0 || c>=cols) return;
-         if(grid[r][c]== '0')  return;
+    int rows, cols;
+    void bfs(vector<vector<char>>& grid, int r, int c) {
+        queue<pair<int, int>> q;
+        q.push({r, c});
 
-        grid[r][c] ='0';
-        dfs(grid,r+1,c); //down vertical
-        dfs(grid,r-1,c); //up vertical
-        dfs(grid,r,c+1); //right horizontal
-        dfs(grid,r,c-1); //left horizontal
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
 
+            int row = node.first;
+            int col = node.second;
 
-    }
+            if (row < 0 || row >= rows || col < 0 || col >= cols)
+                continue;
+
+            if (grid[row][col] == '0')
+                continue;
+
+            grid[row][col] = '0';
+
+            q.push({row + 1, col}); // down
+            q.push({row - 1, col}); // up
+            q.push({row, col + 1}); // right
+            q.push({row, col - 1}); // left
+        };
+    };
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()) return 0;
+        if (grid.empty())
+            return 0;
         rows = grid.size();
         cols = grid[0].size();
         int count = 0;
-
-        for(int i=0; i<rows; i++ ){
-            for(int j=0;j<cols; j++){
-                if(grid[i][j]=='1'){
-                   ++count;
-                   dfs(grid,i,j);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    ++count;
+                    bfs(grid, i, j);
                 }
             }
         }
-    return count;  
+
+        return count;
     }
 };
