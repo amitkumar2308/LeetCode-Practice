6a30b54b4dfee9cfd9a6ef6871b07b4f07f02512
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-16 19:39:47 +0530

    .cpp 0743-network-delay-time Medium 05-16-2026 91 ms (34.18%) 44.4 MB (67.29%)

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>dist(n+1,INT_MAX);
-
-        unordered_map<int,vector<pair<int,int>>>adj; //src -> (target,time);
-
-        for(vector<int> &vec : times){
+        vector<int> dist(n + 1, INT_MAX);
+        unordered_map<int, vector<pair<int, int>>> adj;
+        for (vector<int>& vec : times) {
             int src = vec[0];
             int target = vec[1];
             int time = vec[2];
-
-            adj[src].push_back({target,time});
+            adj[src].push_back({target, time});
         }
 
-        queue<int>q;
         dist[k] = 0;
+        queue<int>q;
         q.push(k);
 
         while(!q.empty()){
-
             int u = q.front();
             q.pop();
 
@@ -31,16 +27,13 @@ public:
                     q.push(v);
                 }
             }
-            
         }
-
         int maxTime = 0;
-
         for(int i=1; i<=n; i++){
-            if(dist[i]==INT_MAX) return -1;
+            if(dist[i]==INT_MAX)return -1;
             maxTime = max(maxTime,dist[i]);
-        }
-        
-    return maxTime;
+
+        }  
+        return maxTime;  
     }
 };
