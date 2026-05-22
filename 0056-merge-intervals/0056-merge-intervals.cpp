4a84c5d5ef68ae9f2fb95f30acf551fe83e3e162
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-22 12:51:48 +0530

    .cpp 0056-merge-intervals Medium 05-22-2026 5 ms (52.58%) 23.7 MB (81.36%)

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-          if(intervals.empty()) return {};
-          int n = intervals.size();
-          sort(intervals.begin(),intervals.end());
+           vector<vector<int>>ans;
+           int n =  intervals.size();
+          
 
-          vector<vector<int>>minterval;
-          minterval.push_back(intervals[0]);
+           sort(intervals.begin(),intervals.end());
+            ans.push_back(intervals[0]);
 
-          for(int i=1; i<n; i++){
-
-            vector<int>&curr = intervals[i];
-            vector<int>&last = minterval.back();
-
-            if(last[1]>=curr[0]){
-               last[1] = max(last[1],curr[1]);
-            }else{
-                minterval.push_back(curr);
-            }
-
-          }
-
-    return minterval;
+           for(int i=1; i<n; i++){
+               if(intervals[i][0]<=ans.back()[1]){
+                 ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+               }
+               else{
+                ans.push_back(intervals[i]);
+               }
+           }
+    return ans;
     }
 };
