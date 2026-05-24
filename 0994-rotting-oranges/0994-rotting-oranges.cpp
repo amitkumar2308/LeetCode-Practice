2254a270fd56a6d759fab7175250d66c4e6fd5e9
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-05-24 08:03:20 +0530

    .cpp 0994-rotting-oranges Medium 05-24-2026 0 ms (100%) 16.8 MB (50.87%)

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
+        if(grid.empty()) return 0;
+        int row = grid.size();
+        int col = grid[0].size();
+        
         queue<pair<int,int>>q;
         int fresh = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
                 if(grid[i][j]==2){
-                    q.push({i,j});
+                   q.push({i,j});
                 }
                 if(grid[i][j]==1){
                     fresh++;
@@ -20,32 +20,31 @@ public:
 
         int dx[4] = {1,-1,0,0};
         int dy[4] = {0,0,-1,1};
-        int minutes = 0;
-
+        
+        int min = 0;
         while(!q.empty()&&fresh>0){
             int size = q.size();
 
             while(size--){
-              auto[x,y] = q.front();
-              q.pop();
+                auto[x,y] = q.front();
+                q.pop();
 
-              for(int i=0; i<4; i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
+                for(int i=0; i<4; i++){
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];
 
-                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
-
-                    grid[nx][ny] = 2;
-                    fresh--;
-                    q.push({nx,ny});
+                    if(nx>=0 && ny>=0 && nx<row && ny<col && grid[nx][ny]==1){
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.push({nx,ny});
+                    }
 
                 }
-              }
             }
-        minutes++;
+            min++;
         }
-        if(fresh>0)
-           return -1;
-        return minutes;
+
+        if(fresh>0) return -1;
+        return min;
     }
 };
