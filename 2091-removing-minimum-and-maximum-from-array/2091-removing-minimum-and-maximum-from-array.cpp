e837fee5a8a77600e66a471e9ca47b0e92e2ef61
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-08-30 13:42:37 +0530

    .cpp 2091-removing-minimum-and-maximum-from-array Medium 08-30-2026 0 ms (100%) 92.1 MB (22.14%)

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -3,37 +3,48 @@ public:
     
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
-        int maxi = INT_MIN;
+
+        int maxi = INT_MIN;  
         int mini = INT_MAX;
 
-        for(int i=0; i<n; i++){
-            maxi = max(maxi,nums[i]);
-            mini = min(mini,nums[i]);
+        for(int i =0; i<n; i++){
+            maxi = max(maxi,nums[i]); //maximum
+            mini = min(mini,nums[i]); //minimum
         }
-        
+
+        //indices 
+
         int maxIndex = -1;
         int minIndex = -1;
 
-        // max aur min ke index
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == maxi)
-                maxIndex = i;
-
-            if (nums[i] == mini)
-                minIndex = i;
+        for(int i=0; i<n; i++){
+            if(nums[i]==maxi){
+                maxIndex = i;       //max elemtn index
+            }
+            if(nums[i]==mini){
+                minIndex = i;   // min element index
+            }
         }
 
-        int first = min(maxIndex,minIndex);
+        //which one come first 
+
+        int first  = min(maxIndex,minIndex);
         int last = max(maxIndex,minIndex);
 
+        //3 possibilites
 
+        //bothfront
         int bothFront = last+1;
-        int bothlast = n-first;
-        int oneFrontOneBack = (first+1)+(n-last);
 
-        return min({bothFront,bothlast,oneFrontOneBack});
+        //both from back 
+        int bothBack = n-first;
+
+        //one from front & one from back
+
+        int OFOB = (first+1) + (n-last);
+
 
         
-
+        return min({bothFront,bothBack,OFOB});
     }
 };
