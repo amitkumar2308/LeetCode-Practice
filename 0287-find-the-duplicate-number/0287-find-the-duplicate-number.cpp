35c56b8fb3299e416193ccb1ee4cfc74265e90a6
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2026-09-26 14:27:16 +0530

    .cpp 0287-find-the-duplicate-number Medium 09-26-2026 114 ms (7.49%) 104.6 MB (6.66%)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         unordered_map<int,int>freq;
-        int ans = 0;
         for(int num:nums){
             freq[num]++;
-        }
 
+        }
+        int ans = 0;
         for(auto &it:freq){
             if(it.second>1){
-                ans = it.first;
+               ans = it.first;
             }
         }
-    return ans;
+        return ans;
     }
 };
